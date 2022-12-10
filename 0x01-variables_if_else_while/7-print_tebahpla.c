6c7f0d72059736cdeae390d2b1274d86b3ae4a78
DANIEL-EKWERE <ekweredaniel8@gmail.com>
2022-12-10 13:03:05 -0800

    print

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i <= 'a'; i--)
+		putchar(i);
+	return (0);
+}
