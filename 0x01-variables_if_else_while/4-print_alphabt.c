bd22236c0888610c13e930806054b86f445ac81f
DANIEL-EKWERE <ekweredaniel8@gmail.com>
2022-12-09 18:24:52 -0800

    exclude

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		if (i != q && i != e)
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
