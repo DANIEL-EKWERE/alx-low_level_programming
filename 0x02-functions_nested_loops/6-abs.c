ae9fc5be047059fa5c3fdd76ddaa9207f400a9f1
DANIEL-EKWERE <ekweredaniel8@gmail.com>
2022-12-13 07:57:24 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _abs - check the code
+*@n: charcter to check
+* Return: Always 0 if positive
+*/
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (t);
+	{
+	else
+	{
+		return (-t);
+	}
+}
