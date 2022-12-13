4c4a97af3d00e2eb784afbc6e10210ec9967dd51
DANIEL-EKWERE <ekweredaniel8@gmail.com>
2022-12-13 08:10:21 -0800

    last

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_last_digit - check the code
+* @n:charaxter to check
+* Return: Always 0
+*/
+int print_last_digit(int n)
+{
+	if (n >= 0)
+	{
+		int i = n % 10;
+
+		return (i);
+	}
+}
