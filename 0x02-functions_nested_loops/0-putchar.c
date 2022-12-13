c14d6ae9afe86351ae491b10cd836d48d254a9f3
DANIEL-EKWERE <ekweredaniel8@gmail.com>
2022-12-13 04:44:29 -0800

    forllop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,7 +3,7 @@
 /**
 * main - print _putchar and a new line
 *
-* return: Always 0
+* Return: Always 0
 */
 int main(void)
 {
@@ -12,6 +12,7 @@ int main(void)
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
+	_putchar('h');
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
