32eaa2a4074230b0eb20b1d94c30aa41b5bebe4d
DANIEL-EKWERE <ekweredaniel8@gmail.com>
2022-12-14 11:49:17 -0800

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,17 +18,24 @@ void times_table(void)
 			rep = a * b;
 
 			if (b == 0)
+			{
 				_putchar('0' + rep);
+			}
 			else if (rep < 10)
+			{
 				_putchar(' ');
 				_putchar('0' + rep)
+			}
 			else
+			{
 				_putchar('0' + rep / 10);
 				_putchar('0' + rep % 10);
+			}
 			if (b < 9)
+			{
 				_putchar(',');
 				_putchar(' ');
-
+			}
 			b++;
 		}
 		_putchar('\n');
