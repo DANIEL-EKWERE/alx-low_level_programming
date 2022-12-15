a56144f5d00c1c7851baa22368809b8607707fc0
DANIEL-EKWERE <ekweredaniel8@gmail.com>
2022-12-14 18:52:07 -0800

    fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+* main - chexo the code
+* Return: Always 0
+*/
+int main(void)
+{
+	int a = 0;
+	int b = 1;
+	int next = 0;
+	int sum = 0;
+
+	while (next < 4000000)
+	{
+		next = a + b;
+		a = b;
+		b = next;
+
+		if (next % 2 == 0)
+		{
+			sum += next;
+		}
+	}
+	print("%i\nn", sum);
+	return (0);
+}
