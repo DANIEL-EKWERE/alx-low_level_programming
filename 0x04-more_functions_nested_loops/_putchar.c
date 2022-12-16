2d05376891f218ab97a1db359244ee3a20d8e59c
DANIEL-EKWERE <ekweredaniel8@gmail.com>
2022-12-16 12:21:29 -0800

    is upper

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,9 +1,12 @@
-#include "main.h"                                         #include <unistd.h>
+#include "main.h"
+#include <unistd.h>
 
 /**
-*                                                         * _putchar - write the character c to stdout              *@c: the character to print                               * return: on success 1
+* _putchar - check the code
+* @c: character to be ckecked
+* Return: Always 0
 */
 int _putchar(char c)
 {
-	return (write(1, &c,1));
+	return (write(1, &c, 1));
 }
